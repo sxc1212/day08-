a4e09a310dd25c13b08d579ee9e53f39f84d16e2
sxcsxc123 <sxcsxc12345@proton.me>
2022-07-13 20:46:17 +0800

    所有业务完成，留存

---
+++ common/service_utils/src/main/java/com/atguigu/yygh/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.atguigu.yygh.common.handler;
+
+
+import com.atguigu.yygh.common.GuliException;
+import com.atguigu.yygh.common.R;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+    /**
+     * 统一异常处理类
+     */
+    @ControllerAdvice
+    public class GlobalExceptionHandler {
+
+        @ExceptionHandler(Exception.class)
+        @ResponseBody
+        public R error(Exception e){
+            e.printStackTrace();
+            return R.error();
+        }
+        @ExceptionHandler(ArithmeticException.class)
+        @ResponseBody
+        public R error(ArithmeticException e){
+            e.printStackTrace();
+            return R.error().message("执行了自定义异常");
+        }
+        @ExceptionHandler(GuliException.class)
+        @ResponseBody
+        public R error(GuliException e){
+            e.printStackTrace();
+            return R.error().message(e.getMsg()).code(e.getCode());
+        }
+    }
+
